4a056bf9bbe5aea4f0037f6db3a60a02420f284b
Ben Palmer <bencharlespalmer@gmail.com>
2024-01-30 09:35:04 -0600

    images

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ class Basket(pygame.sprite.Sprite):
         super(Basket,self).__init__()
         self.x = 325
         self.y = 600
-        self.image = pygame.image.load("shoppingcart.png").convert_alpha()
+        self.image = pygame.image.load("basket2.png").convert_alpha()
         self.rect = self.image.get_rect(center = (self.x,self.y))
-        self.image = pygame.transform.scale(self.image, (100,100))
+        self.image = pygame.transform.scale(self.image, (100,70))
 
     def move(self, deltax, deltay):
-        if self.rect.left < 0 or self.rect.right>1200:
+        if self.rect.left < 0 or self.rect.right>650:
             deltax *= -3
         if self.rect.top < 0 or self.rect.bottom > 600:
             deltay *= -3
@@ -22,8 +22,18 @@ class Basket(pygame.sprite.Sprite):
         self.rect.centery += deltay
 
 
+class Chick(pygame.sprite.Sprite):
+    def __init__(self):
+        self.x = random.randint(60,590)
+        self.y = random.randint(60,200)
+        yellow_files = ["yellow/chickwingsdown.png","yellow/chickwingsup.png"]
+        self.pics = [pygame.image.load(img) for img in yellow_files]
+        self.image = self.pics[0]
+        self.rect = self.image.get_rect(center=(self.x,self.y))
 
-
+    def move(self,deltax,deltay):
+        self.rect.centerx += deltax
+        self.rect.centery += deltay
 
 
 
@@ -53,7 +63,9 @@ basket = Basket()
 baskets = pygame.sprite.Group()
 baskets.add(basket)
 
-
+chicks = pygame.sprite.Group()
+for i in range(5):
+    chicks.add(Chick())
 # Main game loop
 running = True
 while running:
@@ -72,14 +84,16 @@ while running:
     if keys[pygame.K_RIGHT]:
         basket.move(2,0)
 
-
+    for chick in chicks:
+        chick.move(0,-1)
     
     baskets.draw(screen)
+    chicks.draw(screen)
     # Update the display
     pygame.display.flip()
 
     # Set a frame rate to 60 frames per second
-    clock.tick(60)
+    clock.tick(30)
 
 # Quit Pygame properly
 pygame.quit()
